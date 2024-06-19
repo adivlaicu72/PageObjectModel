f742c92a32d0a40b367e16d30fdc801431163a8a
Adrian Romulus Vlaicu <adivlaicu72@gmail.com>
2024-06-19 21:55:39 +0300

    curs27ok

--- src/main/java/selenium/utils/BaseTest.java
+++ src/main/java/selenium/utils/BaseTest.java
@@ -1,19 +1,22 @@
 package selenium.utils;
 
-import org.testng.annotations.AfterMethod;
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
-import java.io.*;
-import java.text.SimpleDateFormat;
 
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
+
+import com.google.common.io.Files;
 
 
 
@@ -47,13 +50,14 @@ public class BaseTest {
 		if(result.getStatus() == ITestResult.FAILURE) {
 			
 			TakesScreenshot obj = (TakesScreenshot) driver;
-			File picture = obj.getScreenshotAs(OutputType.FILE)
+			File picture = obj.getScreenshotAs(OutputType.FILE);
 			String timestamp = new SimpleDateFormat("yyyy.MM.dd.hh.mm.ss").format(new Date());
 			
-			
 			try {
+				
 				Files.copy(picture, new File("poze/"+result.getName() +"-"+timestamp+".png"));
-			} catch(Exception e) {
+				
+			}catch(Exception e) {
 				e.printStackTrace();
 			}
 			
